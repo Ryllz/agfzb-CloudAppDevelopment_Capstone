194cbd94519d97e098ff0448347a869523047ddc
ryllz <ryllian@hotmail.com>
2022-03-02 00:57:27 +0000

    update to nodejs files

--- functions/sample/nodejs/index-async-await.js
+++ functions/sample/nodejs/index-async-await.js
@@ -4,7 +4,6 @@
 
  const Cloudant = require('@cloudant/cloudant');
 
-
  async function main(params) {
      const cloudant = Cloudant({
          url: params.COUCH_URL,
@@ -19,4 +18,21 @@
          return { error: error.description };
      }
  
+ }
+
+ let databases = cloudant.db.list();
+ let dealerships_db = cloudant.db.use("dealerships");
+ let reviews_db = cloudant.db.use("reviews");
+
+ async function action_get_all_dealerships(dealerships_db) {
+    try {
+        let all_dealerships = await get_database(dealerships_db);
+        let result = {};
+        all_dealerships.forEach((item) => {
+        result[item.id] = item;
+        });
+        return { json_dealerships: JSON.stringify(result) };
+    } catch (error) {
+        return { error: error.description };
+    }
  }
